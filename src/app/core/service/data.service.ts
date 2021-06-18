5c1a7766d0674c126fb14b354af4fd1373dd8751
KratosvsEzio <jadude19@gmail.com>
2021-06-18 16:59:48 +0500

    api changed

--- src/app/core/service/data.service.ts
+++ src/app/core/service/data.service.ts
@@ -9,12 +9,18 @@ import { shareReplay } from 'rxjs/operators';
 export class DataService {
 
   private apiUrl = 'https://aashir-azeem-game-chart-be.netlify.app/.netlify/functions/app/'; // Netlify api url
-  // private apiUrl = 'http://localhost:8000/'; // Localhost api url
+  // private apiUrl = 'http://localhost:9000/.netlify/functions/app/'; // Localhost api url
 
   constructor(private http: HttpClient) { }
 
-  fetchGames(platform, genre) {
+  select_top_by_playtime(platform, genre) {
     return this.http.get<{code: number, data:Games[], message: string}>(`${this.apiUrl}select_top_by_playtime?genre=${genre}&platform=${platform}`).pipe(
+      shareReplay()
+    );
+  }
+
+  select_top_by_players(platform, genre) {
+    return this.http.get<{code: number, data:Games[], message: string}>(`${this.apiUrl}select_top_by_players?genre=${genre}&platform=${platform}`).pipe(
       shareReplay()
     );
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { DataService } from './../core/service/data.service';
 import { Component, OnInit } from '@angular/core';
 import { map, shareReplay } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { Games } from '../models/games'
  
 @Component({
@@ -32,34 +32,33 @@ export class HomeComponent implements OnInit {
 
   fetchGames() {
     console.log('fetch games')
-    this.games = this.dataSerivce.fetchGames(this.platform, this.genre);
-    this.select_top_by_playtime();
-    this.select_top_by_players();
+    this.gamesGroupByPlaytime$ = this.dataSerivce.select_top_by_playtime(this.platform, this.genre).pipe( map( (res: any) => res.data));
+    this.gamesGroupByPlayer$ = this.dataSerivce.select_top_by_players(this.platform, this.genre).pipe( map( (res: any) => res.data));
+    // this.select_top_by_players();
+    // this.select_top_by_playtime();
   }
 
-  select_top_by_playtime() {
-    this.gamesGroupByPlaytime$ = this.games.pipe(
-      map( (res: any) => {
-        // Filter games array in decending order according to game playtime.
-        return res.data = res.data.sort(function (a, b) {
-          return b.totalPlayTime - a.totalPlayTime;
-        });
-      }),
-      shareReplay(),
-    )
-  }
+  // select_top_by_playtime() {
+  //   this.gamesGroupByPlaytime$ = this.games.pipe(
+  //     map( (res: any) => {
+  //       // Filter games array in decending order according to game playtime.
+  //       return res.data = res.data.sort(function (a, b) {
+  //         return b.totalPlayTime - a.totalPlayTime;
+  //       });
+  //     })
+  //   )
+  // }
 
-  select_top_by_players() {
-    this.gamesGroupByPlayer$ = this.games.pipe(
-      map( (res: any) => {
-        // Filter games array in decending order according to game playtime.
-        return res.data = res.data.sort(function (a, b) {
-          return b.totalPlayers - a.totalPlayers;
-        });
-      }),
-      shareReplay(),
-    )
-  }
+  // select_top_by_players() {
+  //   this.gamesGroupByPlayer$ = this.games.pipe(
+  //     map( (res: any) => {
+  //       // Filter games array in decending order according to game playtime.
+  //       return res.data = res.data.sort(function (a, b) {
+  //         return b.totalPlayers - a.totalPlayers;
+  //       });
+  //     })
+  //   )
+  // }
 
   fetchPlatforms() {
     this.dataSerivce.platforms().subscribe( (res: any) => {
